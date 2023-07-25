55b337c5129d7809fbaeba26a465a07179e6c6d8
C0vpy4 <100491011+C0vpy4@users.noreply.github.com>
2023-07-25 17:19:30 +0300

    Add files via upload

--- target/js/script.js
+++ target/js/script.js
@@ -1,4 +1,53 @@
-const iconClick = document.querySelector(".imgg");
-iconClick.addEventListener("click", function() {
-    document.getElementById("triangle").classList.toggle("animate");
-});
+function gallery() {
+    // Получаем ссылки на изображения
+    const leftImg = document.getElementById('left-img');
+    const topImg = document.getElementById('top-img');
+    const rightImg = document.getElementById('right-img');
+
+    // Создаем анимацию для левого изображения
+    leftImg.style.transition = 'transform 0.5s';
+    leftImg.style.transform = 'translate(-10%)';
+
+    // Создаем анимацию для верхнего изображения
+    topImg.style.transition = 'transform 0.5s';
+    topImg.style.transform = 'translate(-20%)';
+
+    // Создаем анимацию для правого изображения
+    rightImg.style.transition = 'transform 0.5s';
+    rightImg.style.transform = 'translate(-10%)';
+
+    // После завершения анимации меняем местами изображения
+    setTimeout(function() {
+        [leftImg.src, topImg.src, rightImg.src] = [topImg.src, rightImg.src, leftImg.src];
+        leftImg.style.transform = 'translate(0%)';
+        topImg.style.transform = 'translate(0%)';
+        rightImg.style.transform = 'translate(0%)';
+    }, 500); // 500 мил
+}
+
+function gallery_right() {
+    // Получаем ссылки на изображения
+    const leftImg = document.getElementById('left-img');
+    const topImg = document.getElementById('top-img');
+    const rightImg = document.getElementById('right-img');
+
+    // Создаем анимацию для левого изображения
+    leftImg.style.transition = 'transform 0.5s';
+    leftImg.style.transform = 'translate(10%)';
+
+    // Создаем анимацию для верхнего изображения
+    topImg.style.transition = 'transform 0.5s';
+    topImg.style.transform = 'translate(20%)';
+
+    // Создаём анимацию для правого изображения 
+    rightImg.style.transition = 'transform 0.5s';
+    rightImg.style.transform = 'translate(30%)';
+
+    // После завершения анимации меняем местами изображения и сбрасываем анимацию
+    setTimeout(function() {
+        [leftImg.src, topImg.src, rightImg.src] = [topImg.src, rightImg.src, leftImg.src];
+        leftImg.style.transform = 'none';
+        topImg.style.transform = 'none';
+        rightImg.style.transform = 'none';
+    }, 500); // 500 миллисекунд - время анимации (0,5 секунды)
+}
