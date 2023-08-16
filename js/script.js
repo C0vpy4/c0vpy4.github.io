e52d9b24c105f29d755ed1dc89494744e787496b
DeFault <100491011+C0vpy4@users.noreply.github.com>
2023-08-16 22:53:17 +0300

    0.03
    FIX: scroll count, .box position on scroll

--- js/script.js
+++ js/script.js
@@ -1,34 +1,50 @@
-function btnClick() {
-    document.body.style.backgroundImage = "url('./img/back2.png')";
-}
 let count = 0;
 document.addEventListener('wheel', function(event) {
     var y = event.deltaY || event.detail || event.wheelDelta,
         val = 0.1,
         min = 0,
         max = 0;
-    setBg(count);
     if (y > 0) {
         count = count + 1;
+        if (count > 2) {
+            count = 2
+        }
     }
     if (y < 0) {
         count = count - 1;
+        if (count < 0) {
+            count = 0
+        }
     }
+    setBg(count)
 
 
 });
+
+y = 0
+
+function btnClick() {
+    y = y + 1;
+    if (y > 2) {
+        y = 0
+    }
+    setBg(y)
+}
 async function setBg(count) {
     var box = document.getElementsByClassName("box")[0];
     var currentTop = parseInt(box.style.top) || 0;
+    var windowHeight = window.innerHeight;
+    var initialTop = (windowHeight / 2) - (box.offsetHeight / 2); // Вычисляем изначальную позицию по вертикали на середину экрана
     if (count === 0) {
         document.body.style.backgroundImage = "url('./img/back1.png')";
-        box.style.top = 289 + 'px';
+        box.style.top = initialTop + 'px'; // Применяем изначальную позицию
     }
     if (count === 1) {
         document.body.style.backgroundImage = "url('./img/back2.png')";
         box.style.top = (currentTop - 1000) + 'px';
     }
-    if (count === 2) {
+    if (count === 2 | count > 2) {
         document.body.style.backgroundImage = "url('./img/back3.png')";
     }
+
 }
